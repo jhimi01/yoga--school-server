8493eb021caacfd55fdc1bb35af0f1a72ae5c1cd
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-18 19:29:23 +0600

    clean up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
-// This is your test secret API key.
+// This is test secret API key.
 const stripe = require("stripe")(process.env.VITE_SECRETE_PAYMENT);
 
 
@@ -19,7 +19,7 @@ app.use(cors())
 const uri = `mongodb://${process.env.YOGA_SCHOOL}:${process.env.YOGA_SCHOOL_PASSWORD}@ac-ia0nlto-shard-00-00.ysrfscy.mongodb.net:27017,ac-ia0nlto-shard-00-01.ysrfscy.mongodb.net:27017,ac-ia0nlto-shard-00-02.ysrfscy.mongodb.net:27017/?ssl=true&replicaSet=atlas-24z2ze-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+//a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -37,7 +37,6 @@ const verifyJWT = (req, res, next) => {
   const token = authorization.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, decoded) => {
-    // console.log(err, decoded)
     if (err) {
       return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
@@ -50,9 +49,6 @@ const verifyJWT = (req, res, next) => {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
     const userCollection = client.db('yoga-school').collection('users');
     const classCollection = client.db('yoga-school').collection('classes');
     const selectCollect = client.db('yoga-school').collection('selectClass');
@@ -122,7 +118,7 @@ app.get(`/users/instructor`, async(req, res) =>{
 
 
 
-// Get classes posted by a single instructor----------------------
+// Get classes posted by a single instructor
 app.get('/instructors/:email/classes',verifyJWT, async (req, res) => {
   const email = req.params.email;
   const decodedEmail = req.decoded.email
@@ -139,7 +135,7 @@ app.get('/instructors/:email/classes',verifyJWT, async (req, res) => {
 
 
 
-// make instructor to admin ----------------------------
+// make instructor to admin 
 app.patch('/users/admin/:email', async(req, res) =>{
    const email = req.params.email;
    const filter = {email: email}
@@ -153,23 +149,15 @@ app.patch('/users/admin/:email', async(req, res) =>{
 });
 
 
-// add all classes by instructor------------------------
+// add all classes by instructor
 app.post('/users/addclass', async(req, res) =>{
   const body = req.body
   const result = await classCollection.insertOne(body)
   res.send(result)
 })
 
-// get all classes posted by instructor------------------
+// get all classes posted by instructor
 app.get('/users/addclass/', async(req, res) => {
-  // const email = req.params.email;
-  // const decodedEmail = req.decoded.email
-  // // console.log('decode', decodedEmail)
-  // if (email !== decodedEmail) {
-
-  //     return res.status(403).send({error: true, message: 'forbidden access'});
-     
-  // }
   const result = await classCollection.find().toArray();
   res.send(result)
 })
@@ -239,36 +227,19 @@ app.post('/users/selectclass', async(req, res) =>{
   res.send(result)
 })
 
-// -------------------------------------------------------------------------------
-// get selected classess
-// app.get('/users/selectclass/my-class', async (req, res) =>{
-//   // const email= req.params.email;
-//  const email =  req.query.email;
-//  if (!email){
-//   res.send([])
-//  }
-//   const query = {email: email}
-//   const result = await selectCollect.find(query).toArray();
-//   res.send(result)
-// })
-// ----------------------------
+// users selected get only
 app.get(`/users/selectclass/:email`,
  verifyJWT,  
  async (req, res) =>{
 
   const decodedEmail = req.decoded.email
  const email =  req.params.email;
-//  console.log('decodedEmail', decodedEmail)
-//  console.log('Email', email)
-
  if (email !== decodedEmail) {
   return res.status(403).send({error: true, message: 'forbidden access'});
  }
-
  if (!email){
   res.send([])
  }
-
   const query = {email: email}
   const result = await selectCollect.find(query).toArray();
   res.send(result)
@@ -309,19 +280,6 @@ app.get('/enrolled/class/:email', async (req, res) => {
 
 
 // available and enrolled section update
-// app.put('/enrolled/update/:id', async (req, res) => {
-//   const id = req.params.id;
-//   const {availableSeats, Enrolled} = req.body;
-//   const filter = {_id: new ObjectId(id)};
-//   updateDoc = {
-//     $set : {
-//       availableSeats,
-//       Enrolled
-//     },
-//   }
-//   const result = await classCollection.updateOne(filter, updateDoc);
-//   res.send(result);
-// });
 app.put('/enrolled/update/:id', async (req, res) => {
   const id = req.params.id;
   const { availableSeats, Enrolled } = req.body;
@@ -335,9 +293,6 @@ app.put('/enrolled/update/:id', async (req, res) => {
   const result = await classCollection.updateOne(filter, updateDoc);
   res.send(result);
 });
-
-
-
 
 
 
@@ -368,9 +323,6 @@ app.post('/create-payment-intent', async (req, res) =>{
   }
 }
 run().catch(console.dir);
-
-
-
 
 
 app.get('/', (req, res) => {
